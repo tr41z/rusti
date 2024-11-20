be82e2c445808846457a55957bdd68228ae807aa
traizooo <michal.owsiejew23@gmail.com>
2024-11-20 17:41:00 +0000

    Improve error handling in scanner by using match for HTTP requests

--- src-tauri/src/probing/scanner.rs
+++ src-tauri/src/probing/scanner.rs
@@ -11,8 +11,14 @@ pub fn start_scanner(target_ip: String, wordlist_path: String) {
         Ok(wordlist) => {
             for word in wordlist.iter() {
                 let payload: String = construct_payload(&target_ip, word);
-                let res: blocking::Response = blocking::get(&payload).unwrap();
-                scan_results.insert(payload.clone(), res.status());
+                match blocking::get(&payload) {
+                    Ok(res) => {
+                        scan_results.insert(payload.clone(), res.status());
+                    }
+                    Err(_err) => {
+                        eprintln!("Failed to send request to {}. Wrong IP address", payload);
+                    }
+                }
             }
             println!("HashMap with results: {:#?}", scan_results);
         }
