a0adc05f80ffc0e2cd50e2b8c95fb08881018eec
traizooo <michal.owsiejew23@gmail.com>
2024-11-23 11:46:29 +0000

    Refactor scanner functions to use &str for wordlist_path and update .gitignore

--- src-tauri/src/commands/commands.rs
+++ src-tauri/src/commands/commands.rs
@@ -6,6 +6,6 @@ use crate::probing::{enumerator, scanner};
 
 pub fn start_enumerator() {enumerator::start_enumerator();}
 
-pub fn start_scanner(target_ip: IpAddr, target_port: &str, wordlist_path: String, app_handle: AppHandle) {
+pub fn start_scanner(target_ip: IpAddr, target_port: &str, wordlist_path: &str, app_handle: AppHandle) {
     scanner::start_scanner(target_ip, target_port, wordlist_path, app_handle);
 }
--- src-tauri/src/probing/scanner.rs
+++ src-tauri/src/probing/scanner.rs
@@ -5,7 +5,7 @@ use crate::utils::utils::{self, construct_payload};
 use reqwest::blocking;
 use tauri::{AppHandle, Emitter};
 
-pub fn start_scanner(target_ip: IpAddr, target_port: &str, wordlist_path: String, app_handle: AppHandle) {
+pub fn start_scanner(target_ip: IpAddr, target_port: &str, wordlist_path: &str, app_handle: AppHandle) {
     let mut scan_results: HashMap<String, u16> = HashMap::new();
 
     match utils::read_wordlist(&wordlist_path) {
@@ -15,18 +15,6 @@ pub fn start_scanner(target_ip: IpAddr, target_port: &str, wordlist_path: String
                 match blocking::get(&payload) {
                     Ok(res) => {
                         scan_results.insert(payload.clone(), res.status().as_u16());
-                        if res.status().as_u16() == 200 
-                        || res.status().as_u16() == 301 
-                        || res.status().as_u16() == 302
-                        || res.status().as_u16() == 403
-                        || res.status().as_u16() == 401
-                        || res.status().as_u16() == 307 
-                        {
-                            // Emit the URL for preview
-                            app_handle.emit("preview_url", payload.clone()).unwrap_or_else(|e| {
-                                eprintln!("Failed to emit preview URL: {}", e);
-                            });
-                        }
                     }
                     Err(_) => {
                         eprintln!("Failed to send request. Wrong IP address");
@@ -36,7 +24,6 @@ pub fn start_scanner(target_ip: IpAddr, target_port: &str, wordlist_path: String
             app_handle.emit("scan_results", scan_results).unwrap_or_else(|e| {
                 eprintln!("Failed to emit scan results: {}", e);
             });
-
         }
         Err(_) => {
             eprintln!("Failed to read wordlist");
