17b129fbcee76c3b1aa1d520a92029a826366174
traizooo <michal.owsiejew23@gmail.com>
2024-12-02 16:54:55 +0000

    initial subdomain enumeration implementation

--- src-tauri/src/probing/sub_enumerator.rs
+++ src-tauri/src/probing/sub_enumerator.rs
@@ -1,1 +1,13 @@
-pub fn start_enumerator() {}
+use std::{collections::HashMap, net::IpAddr};
+
+use tauri::AppHandle;
+
+pub fn start_enumerator(
+    target_ip: IpAddr,
+    target_port: &str,
+    wordlist_path: &str,
+    app_handle: AppHandle,
+) {
+    // HashMap for storing enumeration results
+    let mut enum_results: HashMap<String, u16> = HashMap::new();
+}
